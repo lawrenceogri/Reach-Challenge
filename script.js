4c0506ffafa33c7553bb77841517c70f2d0aeff2
Lawrence Ogri <lawrenceogri@yahoo.com>
2019-07-28 06:03:40 +0100

    Add markup and script functionality

--- script.js
+++ script.js
@@ -3,24 +3,13 @@ let income = document.querySelector('#income');
 let submit = document.querySelector('#submit');
 let nameText = document.querySelector('#nameText');
 
-nameValue = "";
-incomeValue = 0;
-let arr = submit.onclick(nameValue, incomeValue);
-
-
-submit.onclick = function (a, b) {
-    let data = [];
-    if (submit != null) {
-        a = name.value;
-        b = income.value;
-        data.push(a);
-        data.push(b);
+if (submit != null) {
+    submit.onclick = function () {
+        localStorage.setItem('myName', name.value);
+        localStorage.setItem('myIncome', income.value);
     }
-    return data;
 }
 
-window.onload = function(){
-    if (nameText != null) {
-        nameText.innerHTML = `Here you goes ${data[0]}`;
-    }
+if(nameText != null){
+    nameText.innerHTML = `Here you go ${localStorage.getItem('myName')}`;
 }
